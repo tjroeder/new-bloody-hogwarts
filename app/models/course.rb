d9f80745684d13dfa7a42296a4476ce1c756fdbf
Tim Roeder <timothyjroeder@gmail.com>
2021-12-21 18:55:06 -0700

    Feat: add validations and relationships to models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :student_courses
+  has_many :students, through: :student_courses
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ApplicationRecord
+  validates :name, :age, :house, presence: true
+  
+  has_many :student_courses
+  has_many :courses, through: :student_courses
 end
