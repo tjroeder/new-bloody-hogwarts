053c4ea95802b0ac064e0d9d60099705b4c90267
Tim Roeder <timothyjroeder@gmail.com>
2021-12-21 20:56:21 -0700

    Test: add #student_count

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe Course, type: :model do
     it {should have_many(:students).through(:student_courses)}
   end
 
+  let!(:student_1) { Student.create!(name: "student_1", age: 1, house: "house_1") }
+  let!(:student_2) { Student.create!(name: "student_2", age: 2, house: "house_2") }
+  let!(:student_3) { Student.create!(name: "student_3", age: 3, house: "house_1") }
+  
+  let!(:course_1) { Course.create!(name: "course_1", students: [student_1, student_2]) }
+  let!(:course_2) { Course.create!(name: "course_2", students: [student_1, student_3]) }
+  let!(:course_3) { Course.create!(name: "course_3", students: [student_1]) }
+
+  describe 'instance methods' do
+    describe '#student_count' do
+      it 'counts the number of students in the course' do
+        expect(course_1.student_count).to eq(2)
+        expect(course_2.student_count).to eq(2)
+        expect(course_3.student_count).to eq(1)
+      end
+    end
+  end
 end
