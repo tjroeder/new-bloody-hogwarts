6f1da68dfea642eb8ee7abf0349c4647a5e5ebcb
Tim Roeder <timothyjroeder@gmail.com>
2021-12-21 20:55:56 -0700

    Feat: add #student_count

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,11 @@ class Course < ApplicationRecord
 
   has_many :student_courses
   has_many :students, through: :student_courses
+
+  # Class Methods
+
+  # Instance Methods
+  def student_count
+    students.count
+  end
 end
