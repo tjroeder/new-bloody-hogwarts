d94b38bec48cf96e132f4dafa065b63bbc64b61d
Tim Roeder <timothyjroeder@gmail.com>
2021-12-21 21:25:53 -0700

    Feat: add ::average_age

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,11 @@ class Student < ApplicationRecord
   
   has_many :student_courses
   has_many :courses, through: :student_courses
+
+  # Class Methods
+  def self.average_age
+    average(:age)
+  end
+  
+  # Instance Methods
 end
