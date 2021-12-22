084055317b9c267af93ecff7e18ba988fca0e1ff
Tim Roeder <timothyjroeder@gmail.com>
2021-12-21 21:31:49 -0700

    Test: add ::average_age test with rounding

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe Student, type: :model do
     it {should have_many(:courses).through(:student_courses)}
   end
 
+  let!(:student_1) { Student.create!(name: "name_1", age: 1, house: "house_1") }
+  let!(:student_2) { Student.create!(name: "name_2", age: 2, house: "house_2") }
+  let!(:student_3) { Student.create!(name: "name_3", age: 4, house: "house_1") }
+
+  describe 'class methods' do
+    describe '::average_age' do
+      it 'should return the average student age' do
+        expected = (student_1.age + student_2.age + student_3.age).to_f / 3
+
+        expect(Student.average_age.round(1)).to eq(expected.round(1))        
+      end
+    end
+  end
 end
